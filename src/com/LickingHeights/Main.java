1afe1eeb2bf021194fc8c0fc6b8b3a404eb32896
Asante <kwadwo.agyei.2023@student.lhschools.org>
2020-02-21 12:01:43 -0500

    I messed up the commits...

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -31,8 +31,22 @@ L = 1000 to 100,000,000 years (which will last somewhere between 1000 and 100,00
 
         Scanner userInput;
         userInput = new Scanner(System.in);
-
-        N = rStar * f_p * numberOfExoplanets * f_l * f_i * f_c * L;
+       System.out.println("How many stars have formed? (1 per year.)");
+            rStar = userInput.nextInt();
+       System.out.println("From 0.2 to 0.5, how many of the stars have planets?");
+            f_p = userInput.nextDouble();
+       System.out.println("From 1 to 5, how many of those stars have \n" +
+               "planets capable of developing exoplanets?");
+            numberOfExoplanets = userInput.nextInt();
+       System.out.println("How many of those planets will develop life?(100%)");
+            f_l = userInput.nextInt();
+       System.out.println("How many of those planets will develop intelligent life?");
+            f_i = userInput.nextDouble();
+       System.out.println("From 0.1 to 0.2, how many of those species will be able to communicate?");
+            f_c = userInput.nextDouble();
+       System.out.println("How long will the life last?(1,000 to 100,000,000)");
+            L = userInput.nextInt();
+        N = (int) (rStar * f_p * numberOfExoplanets * f_l * f_i * f_c * L);
         System.out.println("The number of intelligent species that are out there\n " +
                 "and could communicate with us is:"+ N );
 
